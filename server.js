d62115acb48238873adbb335322b28ec671e08d8
fanzhen <2609936633@qq.com>
2016-12-26 11:05:53 +0800

    llala

--- server.js
+++ server.js
@@ -12,21 +12,8 @@ app.use('/',express.static(viewsPath));
 var publicPath=path.join(__dirname,'public');
 app.use('/public',express.static(publicPath));
 <<<<<<< HEAD
-/*app.use('/api',proxy({
-	target:'http://guanjp.com:9805',
-	changeOrigin:true,
-	ws:true,
-	cookieRewrite:true,
-	pathRewrite:{
-		'^/api':'/'
-	}
-}))*/
-app.use('/api',proxy({
-    target:'http://122.10.30.153:9901',
-    pathRewrite:{
-        '^/api':'/'
-    }
-}));
+
+
 app.listen(16905,function(){
 	console.log('duankou16905');
 =======
